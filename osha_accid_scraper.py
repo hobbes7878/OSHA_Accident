b2e00ed20d4900f9ee054a3e3ef81ba31904cb79
unknown <067154@TXGV4HZN1-L.AHC.Belotechnologies.com>
2013-06-24 16:10:03 -0500

    6/24/2013 Changes

--- osha_accid_scraper.py
+++ osha_accid_scraper.py
@@ -13,7 +13,7 @@ import sys
 import string
 from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
-from collections import Counter
+
 
 
 #####################################################
@@ -24,7 +24,7 @@ cur=conn.cursor()
 
 #####################################################
 ## Mechanize Presets ## Nevermine these..
-#####################################################
+#####################################################_
 
 br = mechanize.Browser()
 
@@ -59,17 +59,6 @@ def puncless_string(a_string):
     an_string = ''.join(char for char in a_string if char not in string.punctuation)
     return an_string
 
-#this function dedups a list which will be used for dict keys by adding a suffix
-def dedup_suffix(an_list):
-    counter = Counter()
-    deduped = []
-    for name in an_list:
-        new = name + str(counter[name]) if counter[name] else name
-        counter.update({name: 1})
-        deduped.append(new)
-    return deduped
-
-
 
 def pg_express_writer(table , diction):
     #Get column names from database
@@ -100,25 +89,40 @@ def pg_express_writer(table , diction):
 ##To Dos
 ##############################################################
 
-cur.execute('SELECT \"acc_id\" FROM acc_id_nums;')
-narratives_todo=cur.fetchall()
+cur.execute('SELECT \"activityno\" FROM \"activity_nos_from_1990\";')
+narratives_td_temp=cur.fetchall()
+narratives_td=[tup[0] for tup in narratives_td_temp]
+print len(narratives_td)
 
-cur.execute('SELECT \"accid\" FROM acc_narratives;')
-narratives_scraped=cur.fetchall()
+cur.execute('SELECT \"insp_num\" FROM \"acc_id_nums\";')
+narratives_scraped_temp=cur.fetchall()
+narratives_scraped=[tup[0] for tup in narratives_scraped_temp]
+print len(narratives_scraped)
 
-summary_no_list = [num for num in narratives_todo]
+
+
+
+narratives_todo=list(set(narratives_td)^set(narratives_scraped))
+print "To Do: "+ str(len(narratives_todo))
+
 
 ##############################################################
 ##Scrape accident narratives
 ##############################################################
+
+
 i=1
 stage_stamp=time.clock()
 times=[]
-for num in summary_no_list:
+
+
+for num in narratives_todo[40000:]:
     
 
-    no = str(num[0]) #convert from tuple
-    search_url="http://www.osha.gov/pls/imis/accidentsearch.accident_detail?id="+no
+    no = str(num) #convert from decimal
+
+    search_url="http://www.osha.gov/pls/imis/establishment.inspection_detail?id="+no
+
 
     #chatter
     times.append(time.clock()-stage_stamp)
@@ -126,10 +130,10 @@ for num in summary_no_list:
     sys.stdout.write(stamp)
     stage_stamp=time.clock()
     sys.stdout.write(" > "+no)
-    
-    print " >> "+str(i)+" : "+str(len(summary_no_list))
+    print " >> "+str(i)+" : "+str(len(narratives_todo))
     i+=1
 
+    #open page
     while True:
         try:
             page = br.open(search_url)
@@ -138,56 +142,34 @@ for num in summary_no_list:
             print "Error:",e
             continue
         break
+            
 
     soup = BeautifulSoup(page)
 
-    write_dict={'accid' : no}
+    write_dict={"insp_num":no}
 
-    tables = soup.findAll ('table',{ 'width':'99%','cellpadding':'3','border':'0'})
-    for table in tables:
-
-        headers = table.findAll('td',{'class':'blueBoldTen'})
-        header_list = [puncless_string(str(h.text)).replace(" ","_").lower() for h in headers]
-        header_list=dedup_suffix(header_list)
-        if len(header_list)>0:
-            data = table.findAll('td',{'class':'blueTen'})
-            inspection_dict={}
-            data_list = [str(d.text).replace("'"," ") for d in data]
-            inspection_dict=dict(zip(header_list, data_list))
-        else: #narrative has no heading
-            data = table.findAll('td',{'class':'blueTen'})
-            inspection_dict={}
-            inspection_dict['narrative']=[str(d.text).replace("'"," ") for d in data][0]
-
-
-        write_dict.update(inspection_dict)
-
-    tables = soup.findAll ('table',{ 'width':'99%','cellpadding':'1','border':'0'})
-    for table in tables:
-
-        headers = table.findAll('td',{'class':'blueBoldTen'})
-        header_list = [puncless_string(str(h.text)).replace(" ","_").lower() for h in headers]
-        header_list=dedup_suffix(header_list)
-        data = table.findAll('td',{'class':'blueTen'})
-        data_list = [str(d.text).replace("'"," ") for d in data]
-        inspection_dict={}
-        inspection_dict=dict(zip(header_list, data_list))
-
-        write_dict.update(inspection_dict)
-
+    # tables = soup.findAll ('table',{ 'width':'99%','cellpadding':'2','cellspacing':'2','border':'0'})
+    # for table in tables:
+    tds=soup.findAll('td',{'class':'blueTen'})
+    for td in tds:
         
-    try: #kill null header
-        write_dict['null']=write_dict.pop('') 
-    except:
-        continue
-    
-    #kill null keys
-    db_dict=write_dict.copy()
-    for key in write_dict: 
-        if write_dict[key]=="":
-            db_dict.pop(key)
+        if re.search(r'Summary Nr: (\d{4,})',td.text):
+            match=re.search(r'Summary Nr: (\d{4,})',td.text)
+            write_dict["acc_id"]=str(match.group(1))
 
-    try:
-        pg_express_writer(diction=db_dict, table="acc_narratives")
-    except Exception, e:
-        print "       *Error@ "+no+": "+str(e)
+        if re.search(r'Event: (\d{2}/\d{2}/\d{4})',td.text):
+            match=re.search(r'Event: (\d{2}/\d{2}/\d{4})',td.text)
+            write_dict["event_date"]=str(match.group(1))
+    
+    if len(write_dict) ==1:
+        print "   Matchless"
+        error_file=open('error_file.txt','a')
+        error_file.write(no)
+        error_file.write("\r\n")
+    else:
+        try:
+            pg_express_writer(diction=write_dict, table="acc_id_nums")
+        except Exception, e:
+            print "       *Error@ "+no+": "+str(e)
+
+
